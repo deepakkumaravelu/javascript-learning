6309e2e3ec6cdc1893418665ad8513bf3237f499
deepak <deepak1122003kumar@gmail.com>
2023-08-24 22:09:32 +0530

    practiced dom

--- DOM/script.js
+++ DOM/script.js
@@ -1,1 +1,4 @@
 document.querySelector("h1").innerHTML="Good Bye";
+document.querySelector("p").innerHTML="chandrayaan 3 is successfully landed on moon"
+const element = document.getElementsByTagName("p");
+console.log(element);
