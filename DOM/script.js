65004f0c60adb754c4d8210ad9048f2dfe2a0966
deepak <deepak1122003kumar@gmail.com>
2023-08-27 21:18:31 +0530

    dom practice

--- DOM/script.js
+++ DOM/script.js
@@ -1,4 +1,15 @@
-document.querySelector("h1").innerHTML="Good Bye";
-document.querySelector("p").innerHTML="chandrayaan 3 is successfully landed on moon"
-const element = document.getElementsByTagName("p");
-console.log(element);
+// var heading=document.getElementById("f");
+// heading.innerHTML="hi man!";
+// heading.style.color="green";
+// heading.innerHTML="four";
+// document.lastElementChild.lastElementChild.innerHTML = "Angela"
+// document.getElementsByTagName("li")[2].style.color="purple";
+// document.getElementsByClassName("second")[0].style.color="green";
+// document.getElementById("title").innerHTML="first";
+// document.getElementById("title").style.color="orange";
+document.querySelector("li a").style.color="red";
+document.querySelector("h1").style.textAlign="center";
+document.querySelector("button").style.backgroundColor="yellow";
+document.querySelector("h1").classList.add("huge");
+var nam=document.querySelector("li a").getAttribute("href");
+document.querySelector("h1").innerHTML=nam;
