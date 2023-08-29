488695b04eebde99f3092f12b9e7e69bd1f06d0e
deepak <deepak1122003kumar@gmail.com>
2023-08-29 22:14:38 +0530

    css practice

--- DOM/script.js
+++ DOM/script.js
@@ -1,15 +1,15 @@
-// var heading=document.getElementById("f");
-// heading.innerHTML="hi man!";
-// heading.style.color="green";
-// heading.innerHTML="four";
-// document.lastElementChild.lastElementChild.innerHTML = "Angela"
-// document.getElementsByTagName("li")[2].style.color="purple";
-// document.getElementsByClassName("second")[0].style.color="green";
-// document.getElementById("title").innerHTML="first";
-// document.getElementById("title").style.color="orange";
-document.querySelector("li a").style.color="red";
-document.querySelector("h1").style.textAlign="center";
-document.querySelector("button").style.backgroundColor="yellow";
-document.querySelector("h1").classList.add("huge");
-var nam=document.querySelector("li a").getAttribute("href");
-document.querySelector("h1").innerHTML=nam;
+// // var heading=document.getElementById("f");
+// // heading.innerHTML="hi man!";
+// // heading.style.color="green";
+// // heading.innerHTML="four";
+// // document.lastElementChild.lastElementChild.innerHTML = "Angela"
+// // document.getElementsByTagName("li")[2].style.color="purple";
+// // document.getElementsByClassName("second")[0].style.color="green";
+// // document.getElementById("title").innerHTML="first";
+// // document.getElementById("title").style.color="orange";
+// document.querySelector("li a").style.color="red";
+// document.querySelector("h1").style.textAlign="center";
+// document.querySelector("button").style.backgroundColor="yellow";
+// document.querySelector("h1").classList.add("huge");
+// var nam=document.querySelector("li a").getAttribute("href");
+// document.querySelector("h1").innerHTML=nam;
