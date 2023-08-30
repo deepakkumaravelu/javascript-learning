b97c3bb2c18eea8ef2917a49dc19f25df3d4fa17
Deepak Kumaravelu <67898647+deepakkumaravelu@users.noreply.github.com>
2023-08-30 12:34:50 +0530

    week 3 uploaded

--- week3/script.js
+++ week3/script.js
@@ -1,4 +1,4 @@
-// //task1
+// //task1 Recursion and stack:
 // function recursion(num1){
 //     if(num1==0){
 //     return 1;
@@ -51,3 +51,106 @@
 //     }
 //     var N = 3;
 //     towerOfHanoi(N, 'A', 'C', 'B');
+// //Task 1 JSON and variable length arguments/spread syntax:
+// function add(){
+//     var sum=0;
+//     for(var i=0;i<arguments.length;i++){
+//       sum+=arguments[i];
+//     }
+//     return sum;
+// }
+// console.log(add(1,2,3,4,5));
+// console.log(add(1,2,3,4,5,6));
+// console.log(add(1,2,3,4,5,6,7));
+// //Task 2
+// function sum(...args){
+//   let s=0;
+//   for(let arg of args){
+//     s+=arg;
+//   }
+//   return s;
+// }
+// console.log(sum(1,2,3));
+// // Task 3
+// var s1={
+//   name:"danush",
+//   id:123
+// }
+// var s2=JSON.stringify(s1);
+// s1.name="Rishi";
+// console.log(s2);
+// //Task 4
+// function merge(a,b){
+//   var c={
+//     ...a,
+//     ...b
+//   }
+//   return c;
+// }
+// var a={
+//   name:"Rishi",
+//   id:123
+// }
+// var b={
+//   manager:"danush",
+//   mId:124
+// }
+// //console.log(Object.assign(a,b));
+// //console.log(merge(a,b));
+// // Task 5
+// var obj={
+//   n:"Rishi",
+//   id:33
+// }
+// var str=JSON.stringify(obj);
+// console.log(str);
+// var str1=JSON.parse(str);
+// console.log(str1);
+
+// // Task 1 Closure:
+
+// function funcOne(){
+//   let a=10;
+//   function funcTwo(){
+//     return a*a*4;
+//   }
+//   return funcTwo();
+// }
+// console.log(funcOne());
+
+// // Task 2 
+// let c=10;
+// function counter(){
+//   function inc(){
+//     c=c+1;
+//     return c;
+//   }
+//   return inc();
+// }
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+
+// // Task 3
+// let c=1;
+// let g=1;
+// function counter(){
+//   function inc(){
+//     c=c+1;
+//     function inc1(){
+//       g=g+2;
+//       return g;
+//     }
+//     return inc1()+" "+c;
+//   }
+  
+//   return inc();
+// }
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+// Task 4
+
+
+
+
